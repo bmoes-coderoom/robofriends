ba5e94ab4f4cf249055a57b84d982f7234eb7eed
Bmoe <10567180+bmoes-coderoom@users.noreply.github.com>
2022-03-28 17:57:34 -0400

    Added ErrorBoundary to display error message to end user when unknown error happens in CardList component

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Searchbox from '../components/Searchbox';
 import CardList from '../components/CardList';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 
 
 class App extends Component{
@@ -40,7 +41,9 @@ class App extends Component{
           <h1 className='f1'>RoboFriends</h1>
           <Searchbox searchChange={this.onSearchChange}/>
           <Scroll>
-          <CardList robots={filteredRobots}/>
+            <ErrorBoundary>
+            <CardList robots={filteredRobots}/>
+            </ErrorBoundary>
           </Scroll>
 
         </div>
